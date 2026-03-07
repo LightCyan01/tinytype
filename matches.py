873163ee80d0ac5c2570ecbebb702df35b1bbedd
Jaymie <jaymiearclight@gmail.com>
2026-03-07 07:24:01 -0500

    phase 1

---
+++ matches.py
@@ -0,0 +1,17 @@
+import logging
+import types
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def matches(value, expected_type) -> bool:
+    logger.debug(f"Value: {value}, Expected Type: {getattr(expected_type, "__name__", expected_type)}")
+    logger.debug(f"Expected Type: {expected_type}")
+    
+    if isinstance(expected_type, types.UnionType):
+        logger.debug(expected_type.__args__)
+        return any(matches(value, t) for t in expected_type.__args__)
+        
+    return isinstance(value, expected_type)
+
+print(matches("hi", int | str))
